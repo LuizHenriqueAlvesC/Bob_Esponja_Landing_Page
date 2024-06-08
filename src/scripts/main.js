6d6b322604acc951831aa3440794ae53f3650f42
luiz <luiz980alves@gmail.com>
2024-06-08 20:39:21 -0300

    salvando alterações nas imagens do hero

---
+++ src/scripts/main.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementoDoHeader();
+        } else {
+            exibeElementoDoHeader();
+        }
+
+    })
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+})
+
+function ocultaElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
